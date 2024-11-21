5f537089eaedd02d33adca64d5a96f1427d96d2e
unknown <ilhammaull>
2024-11-21 17:41:35 +0700

    first commit

---
+++ lib/utils/color_utils.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  final secondaryColor = const Color(0xff111111);
+  final primaryColor = const Color(0xff7fff00);
+  final greyColor = Colors.grey;
+  final lightGreyColor = Colors.grey.shade300;
+  final cardColor = const Color(0xff222222);
+  final blackColor = Colors.black;
+  final whiteColor = Colors.white;
+  final bg = Color.fromARGB(255, 46, 185, 245);
+  final bg2 = Color(0xFF203A43);
+}
